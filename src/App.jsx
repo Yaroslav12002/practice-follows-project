5f5068dcebd77edf9e161c4691c183dc46c9ece7
Yaroslav12002 <my.mail12002@gmail.com>
2023-04-26 22:57:42 +0300

    createBrowserRouter include

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import { lazy } from 'react';
 import SharedLayout from './components/SharedLayout';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const Tweets = lazy(() => import('./pages/Tweets/Tweets'));
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<SharedLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="tweets" element={<Tweets />} />
+      <Route path="*" element={<HomePage />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="tweets" element={<Tweets />} />
-          <Route path="*" element={<HomePage />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <RouterProvider router={router} />;
+  // return (
+  //   <>
+  //     <Routes>
+  //       <Route path="/" element={<SharedLayout />}>
+  //         <Route index element={<HomePage />} />
+  //         <Route path="tweets" element={<Tweets />} />
+  //         <Route path="*" element={<HomePage />} />
+  //       </Route>
+  //     </Routes>
+  //   </>
+  // );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { Suspense } from 'react';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <Suspense fallback="Loading...">
-        <App />
-      </Suspense>
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Suspense fallback="Loading...">
+      <App />
+    </Suspense>
+  </React.StrictMode>
 );
