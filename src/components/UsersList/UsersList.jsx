d5f2524c7daff89bcbeb36efaede8a719c4872a0
Yaroslav12002 <my.mail12002@gmail.com>
2023-04-26 23:37:06 +0300

    vite version downgrade

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types';
+// import { useState } from 'react';
 import UserCard from '../UserCard';
 import { List } from './UserList.styled';
 
+// func;
+
 const UserList = ({ users }) => {
+  // const [followedUsers, setFollowedUsers] = useState(
+  //   JSON.parse(localStorage.getItem('followed')) || []
+  // );
+
   return (
     <List>
       {users.map(user => {
-        return <UserCard key={user.id} user={user} />;
+        return (
+          // <UserCard key={user.id} user={user} followed={isFollowed(user.id)} />
+          <UserCard key={user.id} user={user} />
+        );
       })}
     </List>
   );
