3f4768ea10add82dd2fb7770cc9496c64001929a
Yaroslav12002 <my.mail12002@gmail.com>
2023-04-27 14:52:10 +0300

    project removed to other framework

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Suspense } from 'react';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Suspense fallback="Loading...">
-      <App />
+      <BrowserRouter basename="practice-follows-project">
+        <App />
+      </BrowserRouter>
     </Suspense>
   </React.StrictMode>
 );
