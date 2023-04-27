adc4b83089a86e0b3974b0689788c916ba0a7e70
Yaroslav12002 <my.mail12002@gmail.com>
2023-04-27 12:01:05 +0300

    Local storage added

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react';
 import UserCard from '../UserCard';
 import { List } from './UserList.styled';
 
+const LOCALSTORAGE_KEY = 'followed';
+
 const UserList = ({ users }) => {
   const [followedUsers, setFollowedUsers] = useState(
-    JSON.parse(localStorage.getItem('followed')) || []
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || []
   );
 
   const isFollowed = id => {
@@ -13,14 +15,17 @@ const UserList = ({ users }) => {
   };
 
   const handleChangeFollowed = id => {
+    let newFollowedUsers = [];
     if (isFollowed(id)) {
-      const newFollowedUsers = followedUsers.filter(
+      newFollowedUsers = followedUsers.filter(
         followedUser => followedUser !== id
       );
-      setFollowedUsers([...newFollowedUsers]);
-      return;
+    } else {
+      newFollowedUsers = [...followedUsers, id];
     }
-    setFollowedUsers([...followedUsers, id]);
+
+    setFollowedUsers([...newFollowedUsers]);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(newFollowedUsers));
   };
 
   return (
